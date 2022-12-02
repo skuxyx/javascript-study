a55de6d5bb458334a2bed0bf573d0d144bd5fc31
skuxyx <skubaltz@naver.com>
2022-12-02 23:03:40 +0900

    javascript arrow function

--- 4.function/4.expression.js
+++ 4.function/4.expression.js
@@ -7,9 +7,14 @@
 // 화살표 함수
 // const example = () => {};
 
-// 값만 return 하는 함수일 경우 괄호, 리턴 생략 가능.
-// => let add = (a,b) => a+b;
 let add = (a, b) => {
   return a + b;
 };
+
+// 값만 return 하는 함수인 경우 15번 줄과 같이 '괄호', '리턴' 생략 가능.
+// let add = (a, b) => a + b;
 console.log(add(1, 5));
+
+// 파라미터가 하나일 경우 괄호를 생략해서 작성할 수 있음.
+let printNum = (num) => console.log(num);
+printNum(1);
