64079ececefe22deebe2acda2187cbb1df11d5bb
skuxyx <skubaltz@naver.com>
2022-12-02 22:46:14 +0900

    javascript arithmetic study

--- 2.operator/2.arithmetic.js
+++ 2.operator/2.arithmetic.js
@@ -6,11 +6,24 @@ console.log(5 - 2);
 console.log(5 * 2);
 console.log(5 / 2);
 console.log(5 % 2);
-console.log(5 ** 2); // ES7
+console.log(5 ** 2); // ES7 거듭제곱.
 
 // + 더하기 연산자 주의점!
 let text = '두개의' + '글자가';
 console.log(text);
 
-text = '1' + 1; // 문자열과 숫자를 더하면 문자열로 변환된다.
+text = '1' + 1; // 문자열과 숫자를 더하면 문자열로 변환되어서 처리.
+console.log(text); // 11
 console.log(typeof text); // string
+
+// -------------------------------------------------------
+// 그 외(빼기, 곱셈, 나누기)에는
+
+text = '5' - 3;
+console.log(text); // 2
+
+text = '5' * 3;
+console.log(text); // 15
+
+text = '6' / 3;
+console.log(text); // 2
