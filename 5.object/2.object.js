66d82c5196ec7ae50e889380e731531b04ae8553
skuxyx <skubaltz@naver.com>
2022-12-31 23:36:07 +0900

    javascript object

--- 5.object/2.object.js
+++ 5.object/2.object.js
@@ -28,3 +28,32 @@ function deleteObj(obj, key) {
 console.log(deleteObj(obj, 'city'));
 
 console.log(obj);
+
+// ---------------------------------------------------------
+
+const obj2 = {
+  name: 'pratice',
+  age: 20,
+  city: 'yongin',
+};
+
+// Object.keys() => 키 목록을 배열로 반환.
+console.log(Object.keys(obj2)); // [ 'name', 'age', 'city' ]
+
+// 배열을 리턴하므로 반복문을 이용할 수 있음.
+const arr = Object.keys(obj2);
+
+for (let i = 0; i < arr.length; i++) {
+  const key = arr[i];
+  const value = obj2[arr];
+
+  console.log('key => ' + key + ' value => ' + value);
+}
+
+// Object.values() => 객체의 값을 배열로 반환.
+console.log(Object.values(obj2)); // [ 'pratice', 20, 'yongin' ]
+
+// Object.entries() => 객체의 키, 값을 배열로 반환.
+console.log(Object.entries(obj2)); // [ [ 'name', 'pratice' ], [ 'age', 20 ], [ 'city', 'yongin' ] ]
+
+// ---------------------------------------------------------
